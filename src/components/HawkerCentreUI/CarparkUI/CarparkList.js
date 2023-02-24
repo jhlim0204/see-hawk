a6095d68aad802c195c0c922ae8fa86158d17f04
Cheng Yin <chengyinlye@gmail.com>
2023-02-25 01:26:34 +0800

    Temporary Placeholder functions

--- src/components/HawkerCentreUI/CarparkUI/CarparkList.js
+++ src/components/HawkerCentreUI/CarparkUI/CarparkList.js
@@ -1,17 +1,39 @@
 import React, {Component} from 'react';
 import CarkparkDetail from './CarparkDetail';
+import { CarfetchData,CarfetchDataWithQuery,fetchCarParkAvailability } from "../../Utility/APIManager";
 
 class CarkparkList extends Component {
-    render() {
-        return(
-            <>
-            </>
-        )
-    }
+    constructor(props) {
+        super(props);
+        this.state = {
+          data: []
+        };
+      }
 
-    componentDidMount(){
-        /* logic here*/
-    }
+      async componentDidMount() {
+        const data = await fetchCarParkAvailability();
+        this.setState({ data });
+      }
+
+
+   render() {
+    return (
+    //   <div>
+    //     {this.state.data.map((record) => (
+    //       <p key={record._id}>{record.address}</p>
+    //     ))}
+    //   </div>
+    <div>
+        {this.state.data.map((carpark) => (
+          <p key={carpark.carpark_number}>{carpark.carpark_number} - Available Lots: {carpark.carpark_info[0].lots_available}</p>
+        ))}
+      </div>
+
+      
+    );
+  }
+
+   
 }
 
 export default CarkparkList;
