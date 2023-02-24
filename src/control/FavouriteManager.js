557b3e43195dde014b3101a5f1dd58771223571a
dotalim0204 <rockerjunhern@gmail.com>
2023-02-24 22:31:39 +0800

    Update favourite manager

--- src/control/FavouriteManager.js
+++ src/control/FavouriteManager.js
@@ -1,64 +1,50 @@
-import { json } from "react-router-dom";
-import {db, auth, provider} from './firebase.js'
-import {getDoc,doc,updateDoc} from 'firebase/firestore'
+import {db} from './firebase.js'
+import {getDoc, doc, updateDoc} from 'firebase/firestore'
 
 
 export class FavouriteManager{
    
-    static getFavourite(id){
-        const docRef = doc(db,'Accounts',id);
-        var acc;
-        getDoc(docRef)
-            .then((doc)=>{
-                acc = doc.data();
-            })
-      
-       fSet = Set();
-        for (var i=0;i<acc.favList.length;i++){
-            if (!fset.has(acc.favList[i])){
-                fSet.prototype.add(acc.favList[i]);
-            }   
-      }
-      return favSet;
+    static async getFavourite(accountName){
+        const docRef = doc(db, 'Account', accountName);
+        const acc = (await getDoc(docRef)).data;
+        
+        return acc.favList;
     }
 
-    static async addFavourite(id,hID){
-        const docRef = doc(db,'Accounts',id);
-        var favSet = this.getFavourite(id);
-        if (!favSet.has(hID)){
-            favSet.prototype.add(hID);
-            var fList = Array.from(favSet);
-            await updateDoc(docRef,{
-                favList : fList
-            })
-            .then(() => {
-                console.log("Succesfully added favourite");
-            }) 
-            .catch(() => {
-                console.log("Error Adding favourite");
-            })
-            return true;
-    }
-    else return false;
-}
+    static async isFavourite(accountName, hawkerID){
+        const docRef = doc(db, 'Account', accountName);
+        const favouriteList = (await getDoc(docRef)).data();
 
-    static async deleteFavourite(id,hID){
-        const docRef = doc(db,'Accounts',id);
-        let favSet = this.getFavourite(id);
-        if (favSet.has(hID)){
-            favSet.prototype.delete(hID);
-            var fList = Array.from(favSet);
-            await updateDoc(docRef,{
-                favList : fList
-            })
-            .then(() => {
-                console.log("Sucessfully deleted favourite");
-            })
-            .catch(() =>{
-                console.log("Error deleting favourite");
-            })
+        return hawkerID in favouriteList;
+    }
+
+    static async addFavourite(accountName, hawkerID){
+        const docRef = doc(db, 'Account', accountName);
+        await updateDoc(docRef, {
+            favList: arrayUnion(hawkerID)
+        })
+        .then(() => {
+            console.log("Succesfully added favourite");
             return true;
-        }
-        else return false;
-}
+        }) 
+        .catch(() => {
+            console.log("Error Adding favourite");
+            return false;
+        })
+    }
+
+    static async deleteFavourite(accountName, hawkerID){
+        const docRef = doc(db, 'Account', accountName);
+        await updateDoc(docRef, {
+            favList: arrayRemove(hawkerID)
+        })
+        .then(() => {
+            console.log("Sucessfully deleted favourite");
+            return true;
+        })
+        .catch(() =>{
+            console.log("Error deleting favourite");
+            return false;
+        })
+    }
 }
