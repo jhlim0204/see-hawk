7070fe88c18aa305f434ea9c5ddeec707746ff68
HLeong12345 <100272517+HLeong12345@users.noreply.github.com>
2023-02-25 15:52:30 +0800

    Add files via upload

--- src/control/ReviewManager.js
+++ src/control/ReviewManager.js
@@ -1,4 +1,4 @@
-import{db} from './firebase.js';
+import{db} from '../firebase.js';
 import{
     doc, getDoc, updateDoc, setDoc
 } from 'firebase/firestore';//from"https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
